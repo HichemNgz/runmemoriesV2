827b5872eb5b848082f80e6fdb8bb6e20250fced
Hichem Neggaz <101028015+HichemNgz@users.noreply.github.com>
2025-04-30 10:11:30 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-    },
-  };
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals', // optional, useful for Next.js
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
